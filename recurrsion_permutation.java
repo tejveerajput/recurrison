64023140ceed4bb329b646046f1cb2751ac9df32
tejveerajput <tejveersingh275@gmail.com>
2024-06-26 12:56:36 +0530

    Modified a file

--- recurrsion_permutation.java
+++ recurrsion_permutation.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class recurrsion_permutation {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+
+
+    }
+    public static void permu(String st){
+        if(){
+
+        }
+        for(int i=0;i<st.length();i++){
+            char ch=
+        }
+
+
+
+    }
+
 }
